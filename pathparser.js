aa18e7ed37335bf27921cddde0a5fd67f5eee8f0
Kiyatkin Anton <a.kiyatkin@itlf.ru>
2022-04-17 21:15:09 +0400

    fix

--- pathparser.js
+++ pathparser.js
@@ -1,21 +1,21 @@
 const explode = (sep, str) => {
     const i = str.indexOf(sep)
-    return ~i ? [str.substring(0, i), str.substring(i + 1)] : [str]
+    return ~i ? [str.slice(0, i), str.slice(i + 1)] : [str]
 }
 const getExt = (str) => {
     const i = str.lastIndexOf('.')
-    return ~i ? str.substring(i + 1) : ''
+    return ~i ? str.slice(i + 1) : ''
 }
 const getRoot = (str) => {
     const i = str.indexOf('/',1)
-    return ~i ? str.substring(1, i) : ''
+    return ~i ? str.slice(1, i) : ''
 }
 
 export const pathparser = request => {
     //request всегда со слэшом в начале
-    try {
-        request = decodeURI(request)
-    } catch { }
+    request = request.slice(1);
+    try { request = decodeURI(request) } catch { }
+
     const [path, params] = explode('?', request)
     const get = { };
     if (params) {
@@ -27,6 +27,11 @@ export const pathparser = request => {
     }
     const ext = getExt(path)
     const secure = !!~path.indexOf('/.')
-    const root = getRoot(path)
-    return {secure, root, path, ext, get}
+
+    const crumbs = path.split('/')
+    //const root = split[0]
+    //const query = pathar.pop()
+    //path без ведущего слэша
+    //query после последнего слэша
+    return {secure, crumbs, path, ext, get}
 }
